55d97d08698e915b71830770e0993f3f2466a26f
John Edwards <jodoedjr@gmail.com>
2020-11-09 18:21:19 -0500

    working stocks caching in database

--- routes/index.js
+++ routes/index.js
@@ -71,45 +71,53 @@ router.route("/stock")
   .get((req, res, next) => {
     const symbol = req.body.symbol.toString().toUpperCase();
     let interval = 60;
-    
     if (req.body.interval && (req.body.interval === "1" || req.body.interval === "5" || req.body.interval === "15" || req.body.interval === "30")) {
       interval = parseInt(req.body.interval); // replace this with more robust code limited to 1min, 5min, 15min, 30min, and 60min options
     }
-
     //add code to check if the local database has recent information
-db.Stock.findOne({symbol: symbol})
-.then(dbStock => {
-  //figure out how to compare these two
-  console.log(dbStock.updatedAt)
-  console.log(Date.now())
-})
-    //if not, make a new request
-
-    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}min&outputsize=full&apikey=${config.avKey}`)
-      .then(result => {
-        //store data in database
-        const returnedSymbol = result.data["Meta Data"]["2. Symbol"];
-        db.Stock.findOneAndUpdate(
-          { symbol: returnedSymbol }, //filter
-          {
-            symbol: returnedSymbol, //update info
-            data: result.data
-          }, 
-          {upsert: true, new: true, setDefaultsOnInsert: true} // options to create new entry if none found
-          )
-          .then(result => {
-            res.send(result.data)
-          }) // send data to client
-          .catch(err => { // catch any errors that occur
-            res.json(err);
-            console.log(err);
-          })
+    db.Stock.findOne({ symbol: symbol })
+      .then(dbStock => {
+        const oldEntryTime = new Date(Date.now() - (30 * 60 * 1000)); // returns ISO date of 30 minutes ago
+        if (dbStock.updatedAt >= oldEntryTime) { // if database stock info was updated within the last 30 min
+          console.log("sending db info");
+          res.json(dbStock.data);
+        } else {
+          getStockInfo(res, symbol, interval);
+        }
       })
-      .catch(err => {
-        res.json(err);
-        console.log(err);
-      });
+      .catch(err => { // if symbol not found in database
+        getStockInfo(res, symbol, interval);
+      })
   })
+
+const getStockInfo = (res, symbol, interval) => {
+  //if not, make a new request
+  axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}min&outputsize=full&apikey=${config.avKey}`)
+    .then(result => {
+      //store data in database
+      const returnedSymbol = result.data["Meta Data"]["2. Symbol"];
+      db.Stock.findOneAndUpdate(
+        { symbol: returnedSymbol }, //filter
+        {
+          symbol: returnedSymbol, //update info
+          data: result.data
+        },
+        { upsert: true, new: true, setDefaultsOnInsert: true } // options to create new entry if none found
+      )
+        .then(result => {
+          console.log("sending new API info");
+          res.send(result.data)
+        }) // send data to client
+        .catch(err => { // catch any errors that occur
+          res.json(err);
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      res.json(err);
+      console.log(err);
+    });
+}
 
 
 // If no API routes are hit, send the React app
