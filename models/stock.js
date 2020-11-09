7b64f1fa9b1663a0a8122b2a51963ceeb1355cb5
John Edwards <jodoedjr@gmail.com>
2020-11-08 22:36:22 -0500

    Part way done with stock api

---
+++ models/stock.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const stockSchema = new Schema({
+    //id provided by MongoDB
+    symbol: {type: String, required: true},
+    name: {type: String},
+    data: {} // raw data returned by alphavantage API
+}, {timestamps: true});
+
+const Stock = mongoose.model("Stock", stockSchema);
+
+module.exports = Stock;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const router = require("express").Router();
 // const apiRoutes = require("./api");
 const db = require("../models");
+const axios = require("axios");
 
 
 //API routes
@@ -62,12 +63,50 @@ router.route("/api/leaderboard")
   })
 
 // stock api routes
-router.route("/stocks")
+//get single stock values (5 min intervals for a day)
+router.route("/stock")
   //get stock values
   .get((req, res, next) => {
+    const symbol = req.body.symbol.toString().toUpperCase();
+    let interval = 60;
+    if (req.body.interval && (req.body.interval === "1" || req.body.interval === "5" || req.body.interval === "15" || req.body.interval === "30")) {
+      interval = parseInt(req.body.interval); // replace this with more robust code limited to 1min, 5min, 15min, 30min, and 60min options
+    }
 
+    //add code to check if the local database has recent information
+db.Stock.findOne({symbol: symbol})
+.then(dbStock => {
+  //figure out how to compare these two
+  console.log(dbStock.updatedAt)
+  console.log(Date.now())
+})
+    //if not, make a new request
+
+    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}min&outputsize=full&apikey=FGZLKPQ4EWD0K8DO`)
+      .then(result => {
+        //store data in database
+        const returnedSymbol = result.data["Meta Data"]["2. Symbol"];
+        db.Stock.findOneAndUpdate(
+          { symbol: returnedSymbol }, //filter
+          {
+            symbol: returnedSymbol, //update info
+            data: result.data
+          }, 
+          {upsert: true, new: true, setDefaultsOnInsert: true} // options to create new entry if none found
+          )
+          .then(result => {
+            res.send(result.data)
+          }) // send data to client
+          .catch(err => { // catch any errors that occur
+            res.json(err);
+            console.log(err);
+          })
+      })
+      .catch(err => {
+        res.json(err);
+        console.log(err);
+      });
   })
-
 
 
 // If no API routes are hit, send the React app
