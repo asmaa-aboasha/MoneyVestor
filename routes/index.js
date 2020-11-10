4b046d35795cdfd404583a61905d401b796f3d59
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-11-09 21:59:19 -0500

    added authentication and backend code to allow user to login via their google accounts

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
 // const apiRoutes = require("./api");
-const db = require("../models");
+const db = require("../server/models");
 const axios = require("axios");
 const config = require("../config");
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// const mongoose = require("mongoose");
+const routes = require("../routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 //mongodb mongoose models
 const db = require("./models");
 
+const passport = require("../config/passport");
+
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
@@ -20,14 +29,14 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MoneyVestor",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
+// // Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/MoneyVestor",
+//   {
+//     useCreateIndex: true,
+//     useNewUrlParser: true
+//   }
+// );
 
 // Start the API server
 app.listen(PORT, () =>
