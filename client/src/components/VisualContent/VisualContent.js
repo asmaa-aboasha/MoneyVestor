a73844b14e2bb3f6fea9016650fc8aa2a9223516
Asmaa <asmaa.aboasha@gmail.com>
2020-11-12 16:42:51 -0800

    Adding VisualContent.js

---
+++ client/src/components/VisualContent/VisualContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VisualContent.css";
+
+const VisualContent = () => {
+    return (
+        <div>
+            <img src="https://via.placeholder.com/550x450" alt="Image1"></img>
+        </div>
+    )
+}
+
+export default VisualContent;
