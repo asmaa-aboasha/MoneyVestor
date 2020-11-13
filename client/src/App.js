3205c8eec73362f5393c4b60b92df497654844b3
Asmaa <asmaa.aboasha@gmail.com>
2020-11-12 18:08:37 -0800

    Five steps

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import VirtualMarket from './pages/VirtualMarket/VirtualMarket';
 import Home from './pages/Home/Home';
 import NoMatch from './pages/NoMatch';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Headline from './components/Headline/Headline';
 import VisualContent from './components/VisualContent/VisualContent';
 import FiveSteps from './components/FiveSteps/FiveSteps';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 import './App.css';
 
@@ -15,6 +16,9 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <Headline />
+        <VisualContent />
+        <FiveSteps />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/trade" component={VirtualMarket} />
