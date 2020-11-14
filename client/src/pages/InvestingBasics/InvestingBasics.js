8c71810c4f58dc33572bb780555db8bd142c19b1
Asmaa <asmaa.aboasha@gmail.com>
2020-11-14 12:06:34 -0800

    Adding investing basics page

---
+++ client/src/pages/InvestingBasics/InvestingBasics.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { Row } from "react-materialize";
+import './InvestingBasics.css';
+
+// import "materialize-css";
+// import { Row, Col, CardPanel } from "react-materialize";
+
+
+const InvestingBasics = () => {
+    return (
+    
+       <div className="card-one">
+           <div className="card-one-content">
+               <h3>Step 1</h3>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+               <img src="https://via.placeholder.com/300x300" alt="Img1"></img>
+           </div>
+           {/* <div className="card-one-img">
+                <img src="https://via.placeholder.com/300x300" alt="Img1"></img>
+           </div> */}
+       </div>
+       
+
+       
+    );
+};
+
+export default InvestingBasics;
