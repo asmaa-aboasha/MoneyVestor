fddf9cf7e89fd1df9bde2d32d97c2f2001473aed
Asmaa <asmaa.aboasha@gmail.com>
2020-11-19 16:28:33 -0800

    Edited class name

--- client/src/components/Headline/Headline.js
+++ client/src/components/Headline/Headline.js
@@ -6,7 +6,7 @@ const Headline = () => {
         <div className="headline-container">
             <h3 id="headline">The ultimate beginner's guide to stock market investing.</h3>
             <h5 id="subheadline">We'll guide you through the 5 basic steps of investing & give you the experience of what it's like in a free, virtual stock market simulator.</h5>
-            <button class="start-button"><a href="/investing-basics">Get Started</a></button>
+            <button className="start-button"><a href="/investing-basics">Get Started</a></button>
         </div>
     );
 }
